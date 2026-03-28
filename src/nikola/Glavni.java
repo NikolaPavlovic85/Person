7784b4403998987956b4307a14f9ef372445d43b
Nikola Pavlovic <atlantidatempl@gmail.com>
2026-03-28 06:54:49 +0100

    dounjen kod u main klase Glavni

--- src/nikola/Glavni.java
+++ src/nikola/Glavni.java
@@ -5,6 +5,8 @@ public class Glavni {
 	public static void main(String[] args) {
 		Person person = new Person("Ana", 20, false);
         System.out.println(person);
+        person = new Person("Aca", 47, true);
+        System.out.println(person);        
 	}
 
 }
